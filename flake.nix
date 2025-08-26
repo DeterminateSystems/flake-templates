6355770a682d47e18ae04530c4d0eb7c0c231165
Luc Perkins <lucperkins@gmail.com>
2025-08-26 16:03:48 +0100

    Add welcome text for default flake template

--- flake.nix
+++ flake.nix
@@ -9,9 +9,17 @@
           description = "Default flake template";
           path = ./default;
           welcomeText = ''
-            # Your new flake
+            # Welcome to your new Nix flake ❄️🍁 foo
 
-            To get started with your brand new flake...
+            To activate your new flake's development environment, run `nix develop` or `direnv allow` if you use direnv.
+
+            For a more interactive flake initialization experience, delete the `flake.nix` that was just created and use fh, the CLI for FlakeHub:
+
+                nix run "https://flakehub.com/f/DeterminateSystems/fh/0" -- init
+
+            For more on flakes, check out **Zero to Nix**, our flake-forward guide to Nix:
+
+                https://zero-to-nix.com
           '';
         };
       };
