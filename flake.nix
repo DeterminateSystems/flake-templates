545897de103167bd0c1bad8b6c38696cc8bd24c1
Luc Perkins <lucperkins@gmail.com>
2025-08-26 14:51:59 +0100

    Add welcome text for default flake template

--- flake.nix
+++ flake.nix
@@ -8,6 +8,11 @@
         default = {
           description = "Default flake template";
           path = ./default;
+          welcomeText = ''
+            # Your new flake
+
+            To get started with your brand new flake...
+          '';
         };
       };
     };
