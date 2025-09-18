aa7262f47e34a5d0f082c6df7a27673adcb125c7
Luc Perkins <lucperkins@gmail.com>
2025-09-18 16:16:29 +0200

    Make formatting command in inline note more pure

--- default/flake.nix
+++ default/flake.nix
@@ -70,7 +70,7 @@
       # To format all Nix files:
       # git ls-files -z '*.nix' | xargs -0 -r nix fmt
       # To check formatting:
-      # git ls-files -z '*.nix' | xargs -0 -r nixfmt --check
+      # git ls-files -z '*.nix' | xargs -0 -r nix develop --command nixfmt --check
       formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt-rfc-style);
     };
 }
