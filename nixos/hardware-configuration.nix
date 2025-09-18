7f4fe2382c39d9fcb32ae83fedc7672db7633bf3
Luc Perkins <lucperkins@gmail.com>
2025-09-18 15:16:16 +0200

    Suggest using nixos-generate-config

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -8,5 +8,7 @@
 {
   fileSystems."/".device = "/dev/disk/by-label/nixos";
 
-  # Provide any other hardware configuration here
+  # Provide any other hardware configuration here. To automate this process, you can use
+  # `nixos-generate-config`, which comes installed on NixOS. For more information:
+  # https://nixos.org/manual/nixos/stable/#sec-installation-manual-installing
 }
